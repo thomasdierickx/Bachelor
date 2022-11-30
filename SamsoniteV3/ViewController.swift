c9d6b5f22b3924eef065606b90e5b746600a1913
Thomas Dierickx <thomas.dierickx3@student.howest.be>
2022-11-30 15:34:31 +0100

    Done #41, In Progress #35 & #40

--- SamsoniteV3/ViewController.swift
+++ SamsoniteV3/ViewController.swift
@@ -15,7 +15,15 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     var modelScene: SCNScene?
     var modelSceneNode: SCNNode?
+    var modelSceneOpen: SCNScene?
+    var modelSceneNodeOpen: SCNNode?
     var counter: Double = 0.01;
+    // Intro
+    var node: SCNNode = SCNNode();
+    var nodeBodyText: SCNNode = SCNNode();
+    // Part 1
+    var nodeInstr: SCNNode = SCNNode();
+    var nodeInstrText: SCNNode = SCNNode();
     
     private var hud: MBProgressHUD!
     
@@ -38,40 +46,72 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         modelSceneNode?.eulerAngles = SCNVector3(x: 0, y: 1.5, z: 0)
         modelSceneNode?.position.x = -1
         modelSceneNode?.position.y = -1
+        let materialCase = SCNMaterial();
+        materialCase.diffuse.contents = UIColor.red;
+        modelSceneNode?.geometry?.firstMaterial?.diffuse.contents = UIColor.white
         
         scene.rootNode.addChildNode(modelSceneNode!);
+        
+        modelSceneOpen = SCNScene(named: "IbonOpen.dae");
+        modelSceneNodeOpen = modelSceneOpen?.rootNode.childNode(withName: "IBON", recursively: true)
+        modelSceneNodeOpen?.eulerAngles = SCNVector3(x: 0, y: 1.5, z: 0)
+        modelSceneNodeOpen?.position.x = -1
+        modelSceneNodeOpen?.position.y = -1
+        modelSceneNodeOpen?.isHidden = true
+        
+        scene.rootNode.addChildNode(modelSceneNodeOpen!);
         
         // Create 3D text
         let text = SCNText(string: "Samsonite IBON", extrusionDepth: 2);
         let bodyText = SCNText(string: "De IBON collectie, gelanceerd in 2021, is een pionier \n op het gebied van innovatie en design. De opening bevindt \n zich aan de voorkant van de koffer, waardoor de oppervlakte \n die de valies inneemt hetzelfde blijft wanneer deze geopend is. \n Alle spullen van de reiziger blijven veilig opgeborgen dankzij de \n eenpuntssluiting met geïntegreerd TSA-slot.", extrusionDepth: 1)
         
+        // Intro
         // Create & Add color
         let material = SCNMaterial();
         material.diffuse.contents = UIColor.red;
         material.diffuse.contents = UIFont.systemFont(ofSize: 34, weight: UIFont.Weight.black)
         text.materials = [material];
+        // Creates node object & positions it
+        node.scale = SCNVector3(x: 0.04, y: 0.04, z: 0.04);
+        node.geometry = text;
         
         let materialWhite = SCNMaterial();
         materialWhite.diffuse.contents = UIColor.white;
         material.diffuse.contents = UIFont.systemFont(ofSize: 16, weight: UIFont.Weight.regular)
         bodyText.materials = [materialWhite]
-        
-        // Creates node object & positions it
-        let node = SCNNode();
-//        node.position = SCNVector3(0, -3.0, 0);
-        node.scale = SCNVector3(x: 0.04, y: 0.04, z: 0.04);
-        node.geometry = text;
-        
-        let nodeBodyText = SCNNode();
         nodeBodyText.scale = SCNVector3(x: 0.01, y: 0.01, z: 0.01);
         nodeBodyText.position.y = -1;
         nodeBodyText.geometry = bodyText;
+        
+        // Part 1
+        let titleInstr = SCNText(string: "Instructions", extrusionDepth: 2);
+        titleInstr.materials = [material];
+        nodeInstr.scale = SCNVector3(x: 0.04, y: 0.04, z: 0.04);
+        nodeInstr.position.y = -50;
+        nodeInstr.geometry = titleInstr;
+        
+        let bodyInstrText = SCNText(string: "Swipe right for next text \n Swipe left for previous text \n Move slider to see the real life size \n Tap to change the colors \n Click on the lock to open it \n Experiment and enjoy!", extrusionDepth: 1)
+        bodyInstrText.materials = [materialWhite]
+        nodeInstrText.scale = SCNVector3(x: 0.01, y: 0.01, z: 0.01);
+        nodeInstrText.position.y = -51;
+        nodeInstrText.geometry = bodyInstrText;
         
         // Set the scene & elements to the view
         sceneView.scene = scene
         sceneView.scene.rootNode.addChildNode(node);
         sceneView.autoenablesDefaultLighting = true; // Adds lighting and shadows
         sceneView.scene.rootNode.addChildNode(nodeBodyText);
+        sceneView.scene.rootNode.addChildNode(nodeInstr);
+        sceneView.scene.rootNode.addChildNode(nodeInstrText);
+        
+        let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(Swiping(_:)))
+        let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(Swiping(_:)))
+
+        leftSwipe.direction = .left
+        rightSwipe.direction = .right
+        
+        sceneView.addGestureRecognizer(leftSwipe)
+        sceneView.addGestureRecognizer(rightSwipe)
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
@@ -102,11 +142,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
-    
-    @IBAction func TapModel(_ sender: UITapGestureRecognizer) {
-        print("you have tapped the screen");
-    }
-    
     @IBAction func PinchGestureRecognizer(_ sender: UIPinchGestureRecognizer) {   guard sender.view != nil else { return }
         // Zoomable case
 //        if sender.state == .began || sender.state == .changed {
@@ -118,29 +153,55 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     @IBAction func sliderScale(_ sender: UISlider) {
         sender.minimumValue = 0.001
-        sender.maximumValue = 1.0
+        sender.maximumValue = 0.021
         print(sender.value)
         modelSceneNode?.scale = SCNVector3(sender.value, sender.value, sender.value);
+        modelSceneNodeOpen?.scale = SCNVector3(sender.value, sender.value, sender.value);
     }
     
-    @IBAction func ChangePosition(_ sender: UILongPressGestureRecognizer) {
-//        let touch = sender.location(in: sceneView)
-        
-//        Wordt momenteel niet gebruikt
-//        let hitTestResults = sceneView.hitTest(touch, types: .existingPlane)
-        
-//        modelSceneNode?.position = SCNVector3(touch.x / 100, -3.0, touch.y / 100)
+    
+    @IBAction func SliderRotation(_ sender: UISlider) {
+        sender.minimumValue = 0
+        sender.maximumValue = 6.2
+        print(sender.value)
+        modelSceneNode?.eulerAngles = SCNVector3(x: 0, y: sender.value, z: 0)
+        modelSceneNodeOpen?.eulerAngles = SCNVector3(x: 0, y: sender.value, z: 0)
     }
+    
+    @IBAction func Swiping(_ sender: UISwipeGestureRecognizer) {
+        let arrNodes = [node, nodeBodyText, nodeInstr, nodeInstrText];
+        
+        switch sender.direction{
+            case .left:
+                for arrNode in arrNodes {
+                    arrNode.position.y = arrNode.position.y - 50
+                }
+            case .right:
+                for arrNode in arrNodes {
+                    arrNode.position.y = arrNode.position.y + 50
+                }
+            default://default
+                print("default")
+            }
+    }
+    
+    @IBAction func OpenCase(_ sender: UITapGestureRecognizer) {
+        if modelSceneNode?.isHidden == true && modelSceneNodeOpen?.isHidden == false{
+            modelSceneNode?.isHidden = false
+            modelSceneNodeOpen?.isHidden = true
+        } else {
+            modelSceneNode?.isHidden = true
+            modelSceneNodeOpen?.isHidden = false
+        }
+    }
+    
+    
+    @IBAction func ChangeColor(_ sender: UIButton) {
+        print("test")
+    }
+    
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
